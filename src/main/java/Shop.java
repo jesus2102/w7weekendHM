7d9b6d2673b323553fb4419415dc59bd18155900
jesus2102 <jesusperezmarin@gmail.com>
2018-07-06 17:40:47 +0100

    ShopTest created. countItems Test passed

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
+import Shop.Item;
 
 public class Shop {
 
-    ArrayList<Shop.Item> items;
+    private ArrayList<Item> items;
 
-    public Shop(ArrayList<Shop.Item> items){
-        this.items = items;
+    public Shop(){
+        this.items = new ArrayList<>();
+    }
+
+    public int countItems() {
+        return this.items.size();
     }
 }
---
+++ src/test/java/ShopTest.java
@@ -0,0 +1,28 @@
+import Instruments.Guitar;
+import Instruments.Piano;
+import OtherItems.GuitarString;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopTest {
+
+    Shop shop;
+    Guitar guitar;
+    GuitarString guitarString;
+    Piano piano;
+
+    @Before
+    public void before(){
+        shop = new Shop();
+        guitar = new Guitar(6, "Accoustic", 160.00, 270.00, "AL-230");
+        piano = new Piano(64, true, "Classic", 450.00, 680.00, "Yamaha 920L");
+        guitarString = new GuitarString("Accoustic", 2.00, 4.00);
+    }
+
+    @Test
+    public void canCountItems(){
+        assertEquals(0, shop.countItems());
+    }
+}
