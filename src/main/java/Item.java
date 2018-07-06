9a40dc77b9a136c7724eef7230b61de1bab3555c
jesus2102 <jesusperezmarin@gmail.com>
2018-07-06 15:45:08 +0100

    Initial commit. Abstract class Item Created

---
+++ src/main/java/Item.java
@@ -0,0 +1,14 @@
+public abstract class Item {
+
+    private String type;
+    private double priceBuyed;
+    private double priceSold;
+
+    public Item(String type, double priceBuyed, double priceSold){
+        this.type = type;
+        this.priceBuyed = priceBuyed;
+        this.priceSold = priceSold;
+
+    }
+
+}
